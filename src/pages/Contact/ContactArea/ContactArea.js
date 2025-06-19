5a16f3855dace8fd50186101f0b66000fae3eff3
francaboy <srdjan.msevic@gmail.com>
2025-06-19 11:59:06 +0200

    update
    added new contact info...

--- src/pages/Contact/ContactArea/ContactArea.js
+++ src/pages/Contact/ContactArea/ContactArea.js
@@ -15,6 +15,7 @@ const ContactArea = () => {
                 <h3>E-mail</h3>
                 <p>info@biotega.net</p>
                 <p>hr@biotega.net</p>
+                 <p>servis@biotega.net</p> 
               </div>
             </div>
             <div className="col-xl-4 col-lg-4 col-md-4">
@@ -22,7 +23,7 @@ const ContactArea = () => {
                 <i className="fas fa-map-marker-alt"></i>
                 <h3>Lokacija</h3>
                 <p>
-                  Sime Miljuša bb, 78000 Banja Luka, <br></br> Bosna i
+                  Sime Miljuša bb,<br></br> 78000 Banja Luka, <br></br> Bosna i
                   Hercegovina
                 </p>
               </div>
@@ -31,8 +32,9 @@ const ContactArea = () => {
               <div className="contact text-center mb-30">
                 <i className="fas fa-phone"></i>
                 <h3>Telefon</h3>
-                <p>+387 51 439 406</p>
-                <p>+387 51 439 392</p>
+                <p>Centrala: +387 51 439 406</p>
+                <p>Prodaja: 051/439-405</p>
+                <p>Fax: 051/439-392</p>
               </div>
             </div>
           </div>
